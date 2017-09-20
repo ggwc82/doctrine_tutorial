7ee0b45147c0cd023791f77ca6bac60a9597eca6
Godfrey Chiu <godfreychiu@Godfreys-MacBook-Pro.local>
2017-09-20 09:12:44 +0100

    Added Bug and User php files

---
+++ src/Bug.php
@@ -0,0 +1,99 @@
+<?php
+// src/Bug.php
+
+use Doctrine\Common\Collections\ArrayCollection;
+
+
+/**
+ * @Entity(repositoryClass="BugRepository") @Table(name="bugs")
+ */
+class Bug
+{
+    /**
+     * @Id @Column(type="integer") @GeneratedValue
+     * @var int
+     */
+    protected $id;
+    /**
+     * @Column(type="string")
+     * @var string
+     */
+    protected $description;
+    /**
+     * @Column(type="datetime")
+     * @var DateTime
+     */
+    protected $created;
+    /**
+     * @Column(type="string")
+     * @var string
+     */
+    protected $status;
+
+    public function getId()
+    {
+        return $this->id;
+    }
+
+    public function getDescription()
+    {
+        return $this->description;
+    }
+
+    public function setDescription($description)
+    {
+        $this->description = $description;
+    }
+
+    public function setCreated(DateTime $created)
+    {
+        $this->created = $created;
+    }
+
+    public function getCreated()
+    {
+        return $this->created;
+    }
+
+    public function setStatus($status)
+    {
+        $this->status = $status;
+    }
+
+    public function getStatus()
+    {
+        return $this->status;
+    }
+
+    protected $products;
+
+    public function __construct()
+    {
+        $this->products = new ArrayCollection();
+    }
+
+    protected $engineer;
+    protected $reporter;
+
+    public function setEngineer(User $engineer)
+    {
+        $engineer->assignedToBug($this);
+        $this->engineer = $engineer;
+    }
+
+    public function setReporter(User $reporter)
+    {
+        $reporter->addReportedBug($this);
+        $this->reporter = $reporter;
+    }
+
+    public function getEngineer()
+    {
+        return $this->engineer;
+    }
+
+    public function getReporter()
+    {
+        return $this->reporter;
+    }
+}
---
+++ src/User.php
@@ -0,0 +1,55 @@
+<?php
+// src/User.php
+
+use Doctrine\Common\Collections\ArrayCollection;
+
+/**
+ * @Entity @Table(name="users")
+ */
+class User
+{
+    /**
+     * @Id @GeneratedValue @Column(type="integer")
+     * @var int
+     */
+    protected $id;
+    /**
+     * @Column(type="string")
+     * @var string
+     */
+    protected $name;
+
+    public function getId()
+    {
+        return $this->id;
+    }
+
+    public function getName()
+    {
+        return $this->name;
+    }
+
+    public function setName($name)
+    {
+        $this->name = $name;
+    }
+
+    protected $reportedBugs;
+    protected $assignedBugs;
+
+    public function __construct()
+    {
+        $this->reportedBugs = new ArrayCollection();
+        $this->assignedBugs = new ArrayCollection();
+    }
+
+    public function addReportedBug(Bug $bug)
+    {
+        $this->reportedBugs[] = $bug;
+    }
+
+    public function assignedToBug(Bug $bug)
+    {
+        $this->assignedBugs[] = $bug;
+    }
+}
